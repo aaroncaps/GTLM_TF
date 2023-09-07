c9f5181075364ddf907386e23cca5124efe2a4f4
Shafayet <shafayet.islam.61@gmail.com>
2023-09-07 16:22:59 +0930

    navigation mobile view handled

---
+++ js/so_tasks.js
@@ -0,0 +1,19 @@
+function handleHamburger(){
+    let getHamburgerIcon = document.querySelector(".fa-bars");
+    let getXmartIcon = document.querySelector(".fa-xmark");
+    let mobileMenu = document.querySelector(".mobile_menu");
+
+    mobileMenu.style.display = "block";
+    getHamburgerIcon.style.display = "none";
+    getXmartIcon.style.display = "block";
+}
+
+function handleXmark(){
+    let getHamburgerIcon = document.querySelector(".fa-bars");
+    let getXmartIcon = document.querySelector(".fa-xmark");
+    let mobileMenu = document.querySelector(".mobile_menu");
+
+    mobileMenu.style.display = "none";
+    getHamburgerIcon.style.display = "block";
+    getXmartIcon.style.display = "none";
+}
