3efe304578175b1ebf282cd394d242f158b051ea
Aaron Caparas <51281646+aaroncaps@users.noreply.github.com>
2023-09-07 23:09:24 +0930

    updates

---
+++ js/core.js
@@ -0,0 +1,20 @@
+// Used for non desktop devices
+function handleHamburger(){
+    let getHamburgerIcon = document.querySelector(".fa-bars");
+    let getXmartIcon = document.querySelector(".fa-xmark");
+    let mobileMenu = document.querySelector(".mobile_menu");
+
+    mobileMenu.style.display = "block";
+    getHamburgerIcon.style.display = "none";
+    getXmartIcon.style.display = "block";
+}
+
+function handleXmark(){
+    let getHamburgerIcon = document.querySelector(".fa-bars");
+    let getXmartIcon = document.querySelector(".fa-xmark");
+    let mobileMenu = document.querySelector(".mobile_menu");
+
+    mobileMenu.style.display = "none";
+    getHamburgerIcon.style.display = "block";
+    getXmartIcon.style.display = "none";
+}
