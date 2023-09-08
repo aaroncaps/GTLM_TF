c870084937f688015fbc68011a638ef8b9e63b54
Aaron Caparas <51281646+aaroncaps@users.noreply.github.com>
2023-09-08 18:03:29 +0930

    updates again

--- js/supervisor.js
+++ js/supervisor.js
@@ -1,6 +1,4 @@
 function init() {
-    document.getElementById('select-all').addEventListener('change', selectAll);
-    document.getElementById("createGroupBtn").addEventListener("click", confirmCreateGroup);
 }
 init();
 
@@ -13,11 +11,42 @@ function selectAll() {
 }
 
 function confirmCreateGroup() {
-    const confirmation = window.confirm("Are you sure you want to proceed?");
-
-    if (confirmation) {
-        alert("Proceeding with creating the group.");
-    } else {
-        alert("Cancelled.");
+    const groupName = document.getElementById('group-name');
+    const dateFrom = document.getElementById('date-from');
+    const dateTo = document.getElementById('date-to');
+   
+    getCheckedRowsData();
+    
+    if(groupName.value !== '' && dateFrom.value !== '' && dateTo.value !== '') {
+        const confirmation = window.confirm("Confirm create group " + groupName.value + " for the below Security Officers?\n" + getCheckedRowsData());
+        if (confirmation) {
+            document.getElementById("myForm").submit();
+        } else {
+            document.getElementById("myForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+            });
+            
+        }
     }
+    
+}
+
+function getCheckedRowsData() {
+    
+    const checkedRowsData = [];
+    const checkboxes = document.querySelectorAll('.checkbox');
+    checkboxes.forEach(function (checkbox, index) {
+        if (checkbox.checked) {
+            const row = checkbox.closest('tr');
+            const userId = row.querySelector('td:nth-child(2)').textContent;
+            const name = row.querySelector('td:nth-child(3)').textContent;
+            checkedRowsData.push({ userId, name });
+        }
+    });
+     // Generate a message with the checked IDs and Names
+     const message = checkedRowsData.map(function (data) {
+        return `   ${data.userId} ${data.name}`;
+    }).join('\n');
+    
+    return message;
 }
