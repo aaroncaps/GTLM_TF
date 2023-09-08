a92629f793cb153f01f5141b1a4b02562d1a6d1d
Aaron Caparas <51281646+aaroncaps@users.noreply.github.com>
2023-09-08 20:29:53 +0930

    updates

--- js/supervisor.js
+++ js/supervisor.js
@@ -1,24 +1,20 @@
-function init() {
-}
-init();
-
-function selectAll() {
-    const selectAllCheckbox = document.getElementById('select-all');
-    const checkboxes = document.querySelectorAll('.checkbox');
+//tableId: pass in the id of the table
+function selectAllCheckboxes(tableId) {
+    const selectAllCheckbox = document.getElementById(`selectAll${tableId.charAt(tableId.length - 1)}`);
+    const checkboxes = document.querySelectorAll(`#${tableId} tbody input[type="checkbox"]`);
     checkboxes.forEach(checkbox => {
         checkbox.checked = selectAllCheckbox.checked;
     });
 }
 
-function confirmCreateGroup() {
+//tableId: pass in the id of the table
+function confirmCreateGroup(tableId) {
     const groupName = document.getElementById('group-name');
     const dateFrom = document.getElementById('date-from');
     const dateTo = document.getElementById('date-to');
-   
-    getCheckedRowsData();
     
     if(groupName.value !== '' && dateFrom.value !== '' && dateTo.value !== '') {
-        const confirmation = window.confirm("Confirm create group " + groupName.value + " for the below Security Officers?\n" + getCheckedRowsData());
+        const confirmation = window.confirm("Confirm create group " + groupName.value + " for the below Security Officers?\n" + getCheckedRowsData(tableId));
         if (confirmation) {
             document.getElementById("myForm").submit();
         } else {
@@ -28,25 +24,72 @@ function confirmCreateGroup() {
             
         }
     }
-    
 }
 
-function getCheckedRowsData() {
+function confirmUpdateGroup(tableId) {
+    const groupName = document.getElementById('group-name');
     
+    if(groupName.value !== '') {
+        const confirmation = window.confirm("Confirm add Security Officer below to " + groupName.value + " group?\n" + getCheckedRowsData(tableId));
+        if (confirmation) {
+            document.getElementById("myForm").submit();
+        } else {
+            document.getElementById("myForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+            });
+            
+        }
+    }
+}
+
+function confirmDeleteGroup(tableId) {
+    const groupName = document.getElementById('group-name');
+    
+    if(groupName.value !== '') {
+        const confirmation = window.confirm("Confirm delete  " + groupName.value + " group?");
+        if (confirmation) {
+            document.getElementById("myForm").submit();
+        } else {
+            document.getElementById("myForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+            });
+            
+        }
+    }
+}
+
+function confirmDeleteSO(tableId) {
+    const groupName = document.getElementById('group-name');
+    
+    if(groupName.value !== '') {
+        const confirmation = window.confirm("Confirm delete Security Officer from  " + groupName.value + " group?\n" + getCheckedRowsData(tableId));
+        if (confirmation) {
+            document.getElementById("myForm").submit();
+        } else {
+            document.getElementById("myForm").addEventListener("submit", function(event) {
+                event.preventDefault();
+            });
+            
+        }
+    }
+}
+
+function getCheckedRowsData(tableId) {
     const checkedRowsData = [];
-    const checkboxes = document.querySelectorAll('.checkbox');
-    checkboxes.forEach(function (checkbox, index) {
+    const checkboxes = document.querySelectorAll(`#${tableId} tbody input[type="checkbox"]`);
+
+    checkboxes.forEach(checkbox => {
         if (checkbox.checked) {
             const row = checkbox.closest('tr');
-            const userId = row.querySelector('td:nth-child(2)').textContent;
-            const name = row.querySelector('td:nth-child(3)').textContent;
+            const userId = row.cells[1].textContent;
+            const name = row.cells[2].textContent;
             checkedRowsData.push({ userId, name });
         }
     });
-     // Generate a message with the checked IDs and Names
-     const message = checkedRowsData.map(function (data) {
-        return `   ${data.userId} ${data.name}`;
-    }).join('\n');
-    
+    const message = checkedRowsData.map(data => `   ${data.userId} ${data.name}`).join('\n');
     return message;
+}
+
+function cancelButton() {
+    window.location.href = 'groups.html';
 }
